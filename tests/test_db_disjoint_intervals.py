6c2977628e40e39ac7217ff94992b11cbeb62531
ingarantzen <ingar.arntzen@gmail.com>
2025-02-23 12:42:24 +0100

    finished converting db tests to pytest

--- tests/test_db_disjoint_intervals.py
+++ tests/test_db_disjoint_intervals.py
@@ -1,7 +1,6 @@
 """Test script for Disjunct Interval DB."""
 
 from src.server.db_disjoint_intervals import getDB
-import unittest
 
 DB = getDB()
 CHNL = "__test.db__"
@@ -28,98 +27,89 @@ ITEMS = [
 INSERT_ITEM = {"key": "A", "dim": (3.5, 11.5, False, False), "val": "A"}
 
 
-class DbTest(unittest.TestCase):
+def test_all():
 
-    def setUp(self):
-        """Set up testcase."""
-        DB.clear(CHNL)
+    DB.clear(CHNL)
 
-    def tearDown(self):
-        """Tear down testcase."""
-        DB.clear(CHNL)
+    # insert non-overlapping test data
+    DB.update(CHNL, ITEMS)
+    # get all items
+    items = flatten(DB.get(CHNL))
+    assert len(items) == 6
 
-    def test_all(self):
+    # insert new item
+    DB.update(CHNL, INSERT_ITEM)
+    # get all items
+    items = flatten(DB.get(CHNL))
+    assert len(items) == 4
 
-        # insert non-overlapping test data
-        DB.update(CHNL, ITEMS)
-        # get all items
-        items = flatten(DB.get(CHNL))
-        self.assertTrue(len(items) == 6)
+    d = {item["key"]: item for item in items}
+    assert "0" in d
+    assert "1" in d
+    assert "2" not in d
+    assert "3" not in d
+    assert "4" not in d
+    assert "5" in d
+    assert "A" in d
 
-        # insert new item
-        DB.update(CHNL, INSERT_ITEM)
-        # get all items
-        items = flatten(DB.get(CHNL))
-        self.assertTrue(len(items) == 4)
-
-        d = {item["key"]: item for item in items}
-        self.assertTrue("0" in d)
-        self.assertTrue("1" in d)
-        self.assertTrue("2" not in d)
-        self.assertTrue("3" not in d)
-        self.assertTrue("4" not in d)
-        self.assertTrue("5" in d)
-        self.assertTrue("A" in d)
-
-        self.assertTrue(d["1"]["dim"] == (3, 3.5, True, True))
-        self.assertTrue(d["A"]["dim"] == tuple(INSERT_ITEM["dim"]))
-        self.assertTrue(d["5"]["dim"] == (11.5, 12, True, False))
-
-    def test_multiple(self):
-        """
-        Testcase - multiple items in update.
-
-        if A [1,10) is overlayed with two intervals A and C in one operation,
-        The new version of A should replace the old version of A. We denote
-        it AA.
-
-        AA [1,5)
-        C [5, ->]
-
-        First, processing AA leads to A truncated (save right part)
-        A [5, 10)
-
-        Next, processing C leads to A being truncated again (save left part)
-        A [1,5)
-
-        Problem arised if the original A is being used both times, ie.
-        in both iterations.
-
-        This would leads to a sequence of inserts
-
-        A [5, 10) AA[1,5) A[1,5) C[5,->]
-
-        Which imples that AA does not replace AA and is instead lost.
-
-        This was handled by updating the database in each iteration,
-        which is very ineffective.
-
-        A better solution would cache results during iteration and set the
-        database in the correct state once at the end of the iteration.
-
-        This tests this optimization.
-        """
-        # insert item
-        INSERT_ITEM = {"key": "A", "dim": (1, 10, True, False), "val": "A"}
-        DB.update(CHNL, [INSERT_ITEM])
-
-        # insert new items
-        ITEMS = [
-            {"key": "A", "dim": (1, 2, True, False), "val": "AA"},
-            {"key": "C", "dim": (2, 20, True, False), "val": "C"},
-        ]
-        DB.update(CHNL, ITEMS)
-
-        # get result
-        items = flatten(DB.get(CHNL))
-
-        d = {item["key"]: item for item in items}
-        self.assertTrue(len(d) == 2)
-        self.assertTrue(d["A"]["val"] == "AA")
-        self.assertTrue(d["A"]["dim"] == (1.0, 2.0, True, False))
-        self.assertTrue(d["C"]["val"] == "C")
-        self.assertTrue(d["C"]["dim"] == (2.0, 20.0, True, False))
+    assert d["1"]["dim"] == (3, 3.5, True, True)
+    assert d["A"]["dim"] == tuple(INSERT_ITEM["dim"])
+    assert d["5"]["dim"] == (11.5, 12, True, False)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_multiple():
+    """
+    Testcase - multiple items in update.
+
+    if A [1,10) is overlayed with two intervals A and C in one operation,
+    The new version of A should replace the old version of A. We denote
+    it AA.
+
+    AA [1,5)
+    C [5, ->]
+
+    First, processing AA leads to A truncated (save right part)
+    A [5, 10)
+
+    Next, processing C leads to A being truncated again (save left part)
+    A [1,5)
+
+    Problem arised if the original A is being used both times, ie.
+    in both iterations.
+
+    This would leads to a sequence of inserts
+
+    A [5, 10) AA[1,5) A[1,5) C[5,->]
+
+    Which imples that AA does not replace AA and is instead lost.
+
+    This was handled by updating the database in each iteration,
+    which is very ineffective.
+
+    A better solution would cache results during iteration and set the
+    database in the correct state once at the end of the iteration.
+
+    This tests this optimization.
+    """
+    DB.clear(CHNL)
+
+    # insert item
+    INSERT_ITEM = {"key": "A", "dim": (1, 10, True, False), "val": "A"}
+    DB.update(CHNL, [INSERT_ITEM])
+
+    # insert new items
+    ITEMS = [
+        {"key": "A", "dim": (1, 2, True, False), "val": "AA"},
+        {"key": "C", "dim": (2, 20, True, False), "val": "C"},
+    ]
+    DB.update(CHNL, ITEMS)
+
+    # get result
+    items = flatten(DB.get(CHNL))
+
+    d = {item["key"]: item for item in items}
+    assert len(d) == 2
+    assert d["A"]["val"] == "AA"
+    assert d["A"]["dim"] == (1.0, 2.0, True, False)
+    assert d["C"]["val"] == "C"
+    assert d["C"]["dim"] == (2.0, 20.0, True, False)
--- tests/test_intervals.py
+++ tests/test_intervals.py
@@ -1,230 +1,231 @@
-import unittest
 
 import src.server.intervals as intervals
 
 
-class Test(unittest.TestCase):
-
-    def test_order(self):
-        # 4], [4
-        e1 = [4, True, True, False]
-        e2 = [4, False, True, False]
-        res = intervals.endpoint_cmp(e1, e2)
-        self.assertTrue(res == 1)
-
-    def test_itv_cmp(self):
-        a = (2, 4, False, True)
-
-        # b is outside a on the left
-        b = (0, 1, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.OUTSIDE_LEFT)
-
-        # b is overlaps a from left
-        b = (0, 3, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.OVERLAP_LEFT)
-
-        # b is covered by a
-        b = (2.5, 3.5, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.COVERED)
-
-        # b is equal to a
-        b = (2, 4, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.EQUALS)
-
-        # b covers a
-        b = (1.5, 4.5, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.COVERS)
-
-        # b is overlaps a from right
-        b = (3, 6, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.OVERLAP_RIGHT)
-
-        # b is outside a on the right
-        b = (5, 7, False, True)
-        rel = intervals.compare(b, a)
-        self.assertEqual(rel, intervals.IntervalRelation.OUTSIDE_RIGHT)
-
-    def test_normalise(self):
-
-        a1 = [1, 3, True, False]
-        a2 = [3, 4, False, False]        
-        b1 = [1.5, 3.5, True, False]
-        b2 = [3.5, 4.5, False, False]
-        s1 = [3.5, 3.5, True, True]
-        s2 = [4.5, 4.5, True, True]
-
-        # overlap
-        res = intervals.normalise([a1, a2, b1, b2])
-        expect = [[1, 4.5, True, False]]
-        self.assertEqual(res, expect)
-
-        # no overlapp
-        res = intervals.normalise([b1, b2])
-        expect = [b1, b2]
-        self.assertEqual(res, expect)
-
-        # singular
-        res = intervals.normalise([b2, s1, b1, s2])
-        expect = [[1.5, 4.5, True, True]]
-        self.assertEqual(res, expect)
-
-        # duplicates
-        res = intervals.normalise([a1, a2, a1, a2])
-        expect = [a1, a2]
-        self.assertEqual(res, expect)
-
-    def test_set_union(self):
-
-        b1 = [1.5, 3.5, True, False]
-        b2 = [3.5, 4.5, False, False]
-        s1 = [3.5, 3.5, True, True]
-        s2 = [4.5, 4.5, True, True]
-        res = intervals.set_union([b1, b2], [s1, s2])
-        expect = [[1.5, 4.5, True, True]]
-        self.assertEqual(res, expect)
-
-    def test_set_difference(self):
-
-        a1 = [1, 3, True, False]
-        a2 = [3, 4, False, False]        
-        b2 = [3.5, 4.5, False, False]
-        s1 = [2.5, 2.5, True, True]
-
-        res = intervals.set_difference([a1, a2], [s1, b2])
-        expect = [
-            [1, 2.5, True, False],
-            [2.5, 3, False, False],
-            [3, 3.5, False, True]
-        ]
-        self.assertEqual(res, expect)
-
-    def test_set_intersection(self):
-        a1 = [1, 3, True, False]
-        a2 = [3, 4, False, False]        
-        b1 = [1.5, 3.5, True, False]
-        b2 = [3.5, 4.5, False, False]
-
-        res = intervals.set_intersection([a1, a2], [b1, b2])
-        expect = [
-            [1.5, 3, True, False],
-            [3, 3.5, False, False],
-            [3.5, 4, False, False]
-        ]
-        self.assertEqual(res, expect)
-
-    def test_move(self):
-
-        # move intervall
-        olds = [[0, 1000, True, False]]
-        news = [[500, 1500, True, False]]
-
-        exit = intervals.set_difference(olds, news)
-        enter = intervals.set_difference(news, olds)
-        keep = intervals.set_intersection(olds, news)
-
-        self.assertEqual(exit, [[0, 500, True, False]])
-        self.assertEqual(enter, [[1000, 1500, True, False]])
-        self.assertEqual(keep, [[500, 1000, True, False]])
-
-    def test_infinity(self):
-
-        a = [None, None, True, True]
-        b = [1.5, 3.5, True, False]
-        res = intervals.normalise([a, b])
-        self.assertEqual(res, [a])
-
-        res = intervals.set_intersection([a], [b])
-        self.assertEqual(res, [b])
-
-        res = intervals.set_union([a], [b])
-        self.assertEqual(res, [a])
-
-        res = intervals.set_difference([a], [b])
-        self.assertEqual(res, [
-                            [None, 1.5, True, False],
-                            [3.5, None, True, True]
-                        ])
-
-    def test_overlap(self):
-
-        b = (4, 10, True, False)
-
-        # no overlap
-        a = (1, 2, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "keep")
-        self.assertEqual(left, None)
-        self.assertEqual(right, None)
-
-        # overlap to the left
-        a = (1, 6, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, [1, 4, True, False])
-        self.assertEqual(right, None)
-
-        # overlap to the right
-        a = (6, 12, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, None)
-        self.assertEqual(right, [10, 12, True, False])
-
-        # a overlapped by b
-        a = (5, 7, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "drop")
-        self.assertEqual(left, None)
-        self.assertEqual(right, None)
-
-        # a covers b
-        a = (1, 12, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, [1, 4, True, False])
-        self.assertEqual(right, [10, 12, True, False])
-
-        # corner case 1 - a covers b - but empty right interval
-        a = (1, 10, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, [1, 4, True, False])
-        self.assertEqual(right, None)
-
-        # corner case 2 - a covers b - singular right interval
-        a = (1, 10, True, True)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, [1, 4, True, False])
-        self.assertEqual(right, [10, 10, True, True])
-
-        # corner case 3 - a covers b - empty left interval
-        a = (4, 12, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, None)
-        self.assertEqual(right, [10, 12, True, False])
-
-        # corner case 4 - a overlaps b - empty left interval
-        a = (4, 12, False, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "trunc")
-        self.assertEqual(left, None)
-        self.assertEqual(right, [10, 12, True, False])
-
-        # equal
-        a = (4, 10, True, False)
-        op, left, right = intervals.overlay(a, b)
-        self.assertEqual(op, "drop")
-        self.assertEqual(left, None)
-        self.assertEqual(right, None)
+def test_order():
+    # 4], [4
+    e1 = [4, True, True, False]
+    e2 = [4, False, True, False]
+    res = intervals.endpoint_cmp(e1, e2)
+    assert res == 1
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_itv_cmp():
+    a = (2, 4, False, True)
+
+    # b is outside a on the left
+    b = (0, 1, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.OUTSIDE_LEFT
+
+    # b is overlaps a from left
+    b = (0, 3, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.OVERLAP_LEFT
+
+    # b is covered by a
+    b = (2.5, 3.5, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.COVERED
+
+    # b is equal to a
+    b = (2, 4, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.EQUALS
+
+    # b covers a
+    b = (1.5, 4.5, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.COVERS
+
+    # b is overlaps a from right
+    b = (3, 6, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.OVERLAP_RIGHT
+
+    # b is outside a on the right
+    b = (5, 7, False, True)
+    rel = intervals.compare(b, a)
+    assert rel == intervals.IntervalRelation.OUTSIDE_RIGHT
+
+
+def test_normalise():
+
+    a1 = [1, 3, True, False]
+    a2 = [3, 4, False, False]        
+    b1 = [1.5, 3.5, True, False]
+    b2 = [3.5, 4.5, False, False]
+    s1 = [3.5, 3.5, True, True]
+    s2 = [4.5, 4.5, True, True]
+
+    # overlap
+    res = intervals.normalise([a1, a2, b1, b2])
+    expect = [[1, 4.5, True, False]]
+    assert res == expect
+
+    # no overlapp
+    res = intervals.normalise([b1, b2])
+    expect = [b1, b2]
+    assert res == expect
+
+    # singular
+    res = intervals.normalise([b2, s1, b1, s2])
+    expect = [[1.5, 4.5, True, True]]
+    assert res == expect
+
+    # duplicates
+    res = intervals.normalise([a1, a2, a1, a2])
+    expect = [a1, a2]
+    assert res == expect
+
+
+def test_set_union():
+
+    b1 = [1.5, 3.5, True, False]
+    b2 = [3.5, 4.5, False, False]
+    s1 = [3.5, 3.5, True, True]
+    s2 = [4.5, 4.5, True, True]
+    res = intervals.set_union([b1, b2], [s1, s2])
+    expect = [[1.5, 4.5, True, True]]
+    assert res == expect
+
+
+def test_set_difference():
+
+    a1 = [1, 3, True, False]
+    a2 = [3, 4, False, False]        
+    b2 = [3.5, 4.5, False, False]
+    s1 = [2.5, 2.5, True, True]
+
+    res = intervals.set_difference([a1, a2], [s1, b2])
+    expect = [
+        [1, 2.5, True, False],
+        [2.5, 3, False, False],
+        [3, 3.5, False, True]
+    ]
+    assert res == expect
+
+
+def test_set_intersection():
+    a1 = [1, 3, True, False]
+    a2 = [3, 4, False, False]        
+    b1 = [1.5, 3.5, True, False]
+    b2 = [3.5, 4.5, False, False]
+
+    res = intervals.set_intersection([a1, a2], [b1, b2])
+    expect = [
+        [1.5, 3, True, False],
+        [3, 3.5, False, False],
+        [3.5, 4, False, False]
+    ]
+    assert res == expect
+
+
+def test_move():
+
+    # move intervall
+    olds = [[0, 1000, True, False]]
+    news = [[500, 1500, True, False]]
+
+    exit = intervals.set_difference(olds, news)
+    enter = intervals.set_difference(news, olds)
+    keep = intervals.set_intersection(olds, news)
+
+    assert exit == [[0, 500, True, False]]
+    assert enter == [[1000, 1500, True, False]]
+    assert keep == [[500, 1000, True, False]]
+
+
+def test_infinity():
+
+    a = [None, None, True, True]
+    b = [1.5, 3.5, True, False]
+    res = intervals.normalise([a, b])
+    assert res == [a]
+
+    res = intervals.set_intersection([a], [b])
+    assert res == [b]
+
+    res = intervals.set_union([a], [b])
+    assert res == [a]
+
+    res = intervals.set_difference([a], [b])
+    assert res == [
+                    [None, 1.5, True, False],
+                    [3.5, None, True, True]
+                ]
+
+
+def test_overlap():
+
+    b = (4, 10, True, False)
+
+    # no overlap
+    a = (1, 2, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "keep"
+    assert left is None
+    assert right is None
+
+    # overlap to the left
+    a = (1, 6, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left == [1, 4, True, False]
+    assert right is None
+
+    # overlap to the right
+    a = (6, 12, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left is None
+    assert right == [10, 12, True, False]
+
+    # a overlapped by b
+    a = (5, 7, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "drop"
+    assert left is None
+    assert right is None
+
+    # a covers b
+    a = (1, 12, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left == [1, 4, True, False]
+    assert right == [10, 12, True, False]
+
+    # corner case 1 - a covers b - but empty right interval
+    a = (1, 10, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left == [1, 4, True, False]
+    assert right is None
+
+    # corner case 2 - a covers b - singular right interval
+    a = (1, 10, True, True)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left == [1, 4, True, False]
+    assert right == [10, 10, True, True]
+
+    # corner case 3 - a covers b - empty left interval
+    a = (4, 12, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left is None
+    assert right == [10, 12, True, False]
+
+    # corner case 4 - a overlaps b - empty left interval
+    a = (4, 12, False, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "trunc"
+    assert left is None
+    assert right == [10, 12, True, False]
+
+    # equal
+    a = (4, 10, True, False)
+    op, left, right = intervals.overlay(a, b)
+    assert op == "drop"
+    assert left is None
+    assert right is None
